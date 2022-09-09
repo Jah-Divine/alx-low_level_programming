04e3a596cf6f4d4cac4299381c6bda4217ddebd0
Jah-Divine <ihechidaniella@gmail.com>
2022-09-08 17:34:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"
-			programming is like building a multilingual puzzle"
-	    );
+	puts("\" programming is like building a multilingual puzzle");
 	return (0);
 }

c542dbe8f67dfff795acae1702868389cd72069c
Jah-Divine <ihechidaniella@gmail.com>
2022-09-13 14:11:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,14 @@
   **/
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) /
-		sizeof (int);
-	for (count = 0; count < sz; count++)
+	char*sh = "_putchar";
+	while(*sh)
 	{
-		_putchar(str[count]);
+		_putchar(*sh);
+		sh++;
 	}
-	_putchar('n\');
+	_putchar('\n');
+		
 	return (0)
 }
 

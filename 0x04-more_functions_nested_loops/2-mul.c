f567f77ea649721ad55e1aed0b1869741e649419
Jah-Divine <ihechidaniella@gmail.com>
2022-09-15 15:10:09 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b);
+{
+	int multiply = a * b;
+	printf(multiply);
+}
+	int main()
+	{
+		int mul(4,5);
+		return 0;
+	}    

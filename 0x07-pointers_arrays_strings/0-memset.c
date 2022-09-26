219dc865e684760a9d0c4d16ca45c8270092e7d9
Jah-Divine <ihechidaniella@gmail.com>
2022-09-26 12:53:39 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
  * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
 	for (i = 0 ; i < n ; i++)
-	{
+	{ 
 		s[i] = b;
 	}
 	return (s);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-#include <stdio.h>
 
+#include <stdio.h>
 int _putchar(char c);/*_putchar*/
-ar *_memset(char *s, char b, unsigned int n);/*prototype function that fills memory with a constant byte.*/
+char *_memset(char *s, char b, unsigned int n);/*prototype function that fills memory with a constant byte.*/
 char *_memcpy(char *dest, char *src, unsigned int n);/*prototype function that copies memory area.*/
 char *_strchr(char *s, char c);/*prototype function that locates a character in a string.*/
 unsigned int _strspn(char *s, char *accept);/*prototype function that gets the length of a prefix substring.*/

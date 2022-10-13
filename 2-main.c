85a323b520723629f83ed46eaa650920a5d22feb
Jah-Divine <ihechidaniella@gmail.com>
2022-10-13 05:24:58 -0700

    2-main.c

--- 2-main.c
+++ 2-main.c
@@ -1,4 +1,11 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define SIZE 1024
-#endif
+#include <stdio.h>
+/**
+ * * main - Prints the file name of the program
+ *
+ *  Return: Always zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
